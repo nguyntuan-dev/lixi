aa2f6defb465d789ee283df76e33d82073880ffc
nguyntuan-dev <nguyenanhtuan18112006@gmail.com>
2026-02-16 00:11:29 +0700

    Add files via upload

---
+++ nhapkho.js
@@ -0,0 +1,13 @@
+// Firebase config của bạn
+const firebaseConfig = {
+  apiKey: "AIzaSyDYaRY9VUck3yJ8XKFHzBsqOAVK6RxiSzU",
+  authDomain: "lixi-dc4c8.firebaseapp.com",
+  databaseURL: "https://lixi-dc4c8-default-rtdb.firebaseio.com",
+  projectId: "lixi-dc4c8",
+  storageBucket: "lixi-dc4c8.firebasestorage.app",
+  messagingSenderId: "165949952743",
+  appId: "1:165949952743:web:92de227f76ca231035c842"
+};
+
+// Khởi tạo Firebase
+firebase.initializeApp(firebaseConfig);
